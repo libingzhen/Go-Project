1777c4429da005027a9f682bd1a5423af17a793b
Sabri LI <sabri.li@ericsson.com>
2019-01-07 18:20:53 +0800

    update sort 3

---
+++ src/sort-main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//	var a = twoSum([]int{2, 7, 11, 15, 23, 44}, 18)
+	//	fmt.Printf("%v\n", a)
+	//	var b = binarySearch([]int{1, 2, 3, 4, 5, 7, 8, 9, 10}, 4)
+	//	fmt.Printf("%d\n", b)
+	var c = selectionSort([]int{6, 3, 4, 5, 1, 2, 7, 10, 14, 11})
+	fmt.Printf("%d\n", c)
+}
